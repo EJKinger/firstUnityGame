0b88ff68c6dcb0a66fd889a171f50e9d2ed150fa
Eric King <e.king117@gmail.com>
2016-03-07 17:42:07 -0500

    Added up and down key actions

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -7,10 +7,25 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 	  print ("Welcome to Number Wizard!!");
     print ("Pick a number in your head, but don't tell me!");
+    
+    int max = 1000;
+    int min = 1;
+    
+    print ("The highest number you can pick is " + max);
+    print ("The lowest number you can pick is " + min);
+    
+    print ("Is the number higher or lower than 500?");
+    print ("Up arrow for lower, Down arrow for higher and return for equal");
+    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+    if (Input.GetKeyDown (KeyCode.UpArrow)) {
+      print ("Up key was pressed");
+    }
+    if (Input.GetKeyDown (KeyCode.DownArrow)) {
+      print ("Down key was pressed");
+    }
 	}
 }
