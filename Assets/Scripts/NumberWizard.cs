dad2ca24c55fee41dfde9435ca0618307e4d00b7
Eric King <e.king117@gmail.com>
2016-03-09 22:45:41 -0500

    Finished number game

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,30 +2,47 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-
+  
+  int max;
+  int min;
+  int guess;
 	// Use this for initialization
 	void Start () {
-	  print ("Welcome to Number Wizard!!");
-    print ("Pick a number in your head, but don't tell me!");
-    
-    int max = 1000;
-    int min = 1;
-    
-    print ("The highest number you can pick is " + max);
-    print ("The lowest number you can pick is " + min);
-    
-    print ("Is the number higher or lower than 500?");
-    print ("Up arrow for lower, Down arrow for higher and return for equal");
-    
+    startGame ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
     if (Input.GetKeyDown (KeyCode.UpArrow)) {
-      print ("Up key was pressed");
+      min = guess;
+      guess = (max + min) / 2;
+      updateUser ();
     }
-    if (Input.GetKeyDown (KeyCode.DownArrow)) {
-      print ("Down key was pressed");
+    else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+      max = guess;
+      guess = guess / 2;
+      updateUser ();
+    }
+    else if (Input.GetKeyDown (KeyCode.Return)) {
+      print ("I Won!!");
+      startGame ();
     }
 	}
+  
+  void updateUser () {
+    print ("Is the number higher or lower than " + guess + "?");
+    print ("Up arrow for lower, Down arrow for higher and return for equal");
+  }
+  
+  void startGame (){
+    max = 1000;
+    min = 1;
+    guess = 500;
+    print ("Welcome to Number Wizard!!");
+    print ("Pick a number in your head, but don't tell me!");
+    print ("The highest number you can pick is " + max);
+    print ("The lowest number you can pick is " + min);    
+    updateUser ();
+    max = max + 1;
+  }
 }
